9853b22743d5204c840912aee65927b83f6c8a98
Allan Dinakaran <allandinakaran@gmail.com>
2026-05-12 22:51:41 +0530

    Fixed login problem

--- js/login.js
+++ js/login.js
@@ -38,16 +38,3 @@ $("#loginBtn").click(function () {
     });
 
 });
-
-let eyeicon = document.getElementById("eye-close");
-let password = document.getElementById("password");
-
-eyeicon.onclick = function () {
-    if (password.type === "password") {
-        password.type = "text";
-        eyeicon.src = "assets/eye-open.png";
-    } else {
-        password.type = "password";
-        eyeicon.src = "assets/eye-close.png";
-    }
-};
--- js/register.js
+++ js/register.js
@@ -35,16 +35,3 @@ $(document).ready(function () {
         });
     });
 });
-
-let eyeicon = document.getElementById("eye-close");
-let password = document.getElementById("password");
-
-eyeicon.onclick = function () {
-    if (password.type === "password") {
-        password.type = "text";
-        eyeicon.src = "assets/eye-open.png";
-    } else {
-        password.type = "password";
-        eyeicon.src = "assets/eye-close.png";
-    }
-};
