c07e7ff646a53754baf0c878214f9ac4ade59262
Allan Dinakaran <allandinakaran@gmail.com>
2026-06-08 18:57:12 +0530

    Changes to script

--- start.sh
+++ start.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+set -x
+
+echo "PORT=$PORT"
+
 rm -f /etc/apache2/mods-enabled/mpm_event.conf
 rm -f /etc/apache2/mods-enabled/mpm_event.load
 rm -f /etc/apache2/mods-enabled/mpm_worker.conf
 rm -f /etc/apache2/mods-enabled/mpm_worker.load
+
 ln -sf /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
 ln -sf /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
+
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 sed -i "s/*:80>/*:${PORT:-80}>/g" /etc/apache2/sites-enabled/000-default.conf
-echo "PORT=$PORT"
+
 cat /etc/apache2/ports.conf
-grep VirtualHost /etc/apache2/sites-enabled/000-default.conf
+cat /etc/apache2/sites-enabled/000-default.conf
+
+apache2ctl configtest
+
 apache2-foreground
