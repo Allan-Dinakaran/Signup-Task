906177417a5b2d0c3a604439c3b06257e301ffda
Allan Dinakaran <allandinakaran@gmail.com>
2026-06-08 18:52:49 +0530

    Use startup Logs

--- start.sh
+++ start.sh
@@ -7,4 +7,7 @@ ln -sf /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mp
 ln -sf /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 sed -i "s/*:80>/*:${PORT:-80}>/g" /etc/apache2/sites-enabled/000-default.conf
+echo "PORT=$PORT"
+cat /etc/apache2/ports.conf
+grep VirtualHost /etc/apache2/sites-enabled/000-default.conf
 apache2-foreground
